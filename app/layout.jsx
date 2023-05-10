791ed67f3a62908658902dc7fe00b42184029833
DocSJ1980 <docsaj@outlook.com>
2023-05-10 15:48:01 +0500

    Basic Setup done

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+export const metadata = {
+    title: "sjPrompts",
+    description: "Unleash Your Creativity with AI-Powered Prompts: Generate, Share, and Inspire!"
+}
+
+
+const RootLayout = ({ children }) => {
+    return (
+        <html lang='en'>
+            <body className='main'>
+                <div className='gradient'></div>
+            </body>
+            <main className='app'>
+                {children}
+            </main>
+        </html>
+    )
+}
+
+export default RootLayout
