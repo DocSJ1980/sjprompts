bd8c8cab148b91cb9cdd9f70438c8c9e8d148093
DocSJ1980 <docsaj@outlook.com>
2023-05-17 13:29:09 +0500

    Working started on create-prompt form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Form = () => {
+const Form = ({
+    type, post, setPost, submitting, handleSubmit
+}) => {
     return (
-        <div>Form</div>
+        <section className='w-full max-w-full flex-start flex-col'>
+            <h1 className='head_text text-left'>
+                <span className='blue_gradient'>
+                    {type} Post
+                </span>
+            </h1>
+            <p className='desc text-left max-w-md'>
+                {type} and share amazing prompts with the world, and let your imagination run wild with any AI-powered platform.
+            </p>
+            <form onSubmit={handleSubmit} className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'>
+                <label>
+                    <span className='font-satoshi font-semibold text-base text-gray-700'>Your AI Prompt</span>
+                </label>
+
+            </form>
+        </section>
     )
 }
 
