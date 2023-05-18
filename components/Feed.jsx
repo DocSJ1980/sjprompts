07da7eb74f28417b41ff36b114c154d695ae4d7b
DocSJ1980 <docsaj@outlook.com>
2023-05-18 13:28:15 +0500

    PromptCardList component added, data fetched

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
-
+"use client"
+import { useState, useEffect } from 'react'
+import PromptCard from './PromptCard'
 const Feed = () => {
+    const [searchText, setSearchText] = useState("")
+    const [posts, setPosts] = useState([])
+    const handleSearchChange = (e) => {
+
+    }
+    const PromptCardList = ({ data, handleTagClick }) => {
+        console.log('%cFeed.jsx line:11 data', 'color: white; background-color: #007acc;', data);
+        return (
+            <div className='mt-16 prompt_layout'>
+                {data.map((post) => (
+                    <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+                ))}
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch('/api/prompt')
+            const data = await response.json()
+            setPosts(data)
+        }
+        fetchPosts()
+    }, [])
+
+
     return (
-        <div>Feed</div>
+        <section className='feed'>
+            <form className='relative w-full flex-center'>
+                <input
+                    type='text'
+                    placeholder='Search for a tag or a username'
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                    className='search_input peer'
+                />
+            </form>
+            <PromptCardList
+                data={posts || []}
+                handleTagClick={() => { }}
+            />
+        </section>
     )
 }
 
