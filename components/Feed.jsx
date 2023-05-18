5ca6f07a5564054abb8e0f42941bba203d3758f4
DocSJ1980 <docsaj@outlook.com>
2023-05-18 20:47:22 +0500

    Feed and promptcard components implemented

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -12,7 +12,11 @@ const Feed = () => {
         return (
             <div className='mt-16 prompt_layout'>
                 {data.map((post) => (
-                    <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+                    <PromptCard
+                        key={post._id}
+                        post={post}
+                        handleTagClick={handleTagClick}
+                    />
                 ))}
             </div>
         )
