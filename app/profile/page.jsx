cd32095e6f38cc01673a19bd10811b2c06ae4033
DocSJ1980 <docsaj@outlook.com>
2023-05-22 10:55:42 +0500

    edit and delete prompts implemented

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -22,8 +22,19 @@ const MyProfile = () => {
     const handleEdit = (post) => {
         router.push(`/update-prompt?id=${post._id}`)
     }
-    const handleDelete = async () => {
-
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this Prompt?")
+        if (hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`, {
+                    method: 'DELETE'
+                })
+                const filteredPosts = posts.filter((p) => p._id !== post._id)
+                setPosts(filteredPosts)
+            } catch (error) {
+                console.error(error)
+            }
+        }
     }
 
     return (
