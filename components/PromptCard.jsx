a413e53e639b8d6dc211ecf698068b63993ef995
DocSJ1980 <docsaj@outlook.com>
2023-05-25 23:02:02 +0500

    debugged profile error

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,6 +9,13 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     const { data: session } = useSession()
     const pathName = usePathname()
     const router = useRouter()
+    const handleProfileClick = () => {
+        // console.log(post);
+
+        if (post.creator._id === session?.user.id) return router.push("/profile");
+
+        router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+    };
     const handleCopy = () => {
         setCopied(post.prompt)
         navigator.clipboard.writeText(post.prompt)
@@ -19,7 +26,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     return (
         <div className="prompt_card">
             <div className="flex justify-between items-start gap-5">
-                <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+                <div
+                    className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+                    onClick={handleProfileClick}
+                >
                     <Image
                         src={post.creator.image}
                         alt="user_image"
